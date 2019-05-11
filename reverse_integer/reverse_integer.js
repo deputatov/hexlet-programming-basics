27cd58fdeb852ca4f616b06bc0cb9f52e59d8362
Deputatov Nikita <deputatovn@yandex.ru>
2019-05-11 20:39:12 +0300

    deleted func and console.log

--- reverse_integer/reverse_integer.js
+++ reverse_integer/reverse_integer.js
@@ -1,19 +1,7 @@
 //Unicode Character 'RIGHT-TO-LEFT OVERRIDE' (U+202E)
 const reverseInt = (n) => '\u202E' + n;
 
-
 const reverseInt2 = (n) => {
-    let s = String(n);
-    let i = s.length - 1;
-    let r = '';
-    while (i >= 0) {
-      r += s[i];
-      i--;
-    }
-    return Number(r);
-}
-
-const reverseInt3 = (n) => {
     let s = String(Math.abs(n));
     let i = s.length - 1;
     let r = '';
@@ -23,6 +11,3 @@ const reverseInt3 = (n) => {
     }
     return n < 0 ? Number(-r): Number(r);
 }
-//console.log(reverseInt2(-8900));
-console.log(reverseInt3(-122));
-console.log(reverseInt(122));
